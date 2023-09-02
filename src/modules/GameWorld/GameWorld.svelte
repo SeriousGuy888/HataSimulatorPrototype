eeac00f9753ea67e91e600dbdc70f4adfbf2517b
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-09-02 02:22:12 -0400

    split drawing code into separate file

--- src/modules/GameWorld/GameWorld.svelte
+++ src/modules/GameWorld/GameWorld.svelte
@@ -3,6 +3,8 @@
   import type { Tilemap } from "$lib/Tilemap"
   import type { Tile } from "$lib/types"
   import { onMount } from "svelte"
+  import type { View } from "./canvasTypes"
+  import { drawTilemap } from "./tilemapDrawing"
 
   export let tilemap: Tilemap
 
@@ -10,11 +12,12 @@
   const maxZoom = zoomStep ** 8
   const minZoom = zoomStep ** -8
 
-  let view = {
+  let view: View = {
     x: 0,
     y: 0,
     zoom: 1,
   }
+
   let isPanning = false
   let lastMouseX = 0
   let lastMouseY = 0
@@ -67,90 +70,7 @@
     ctx.fillStyle = "#eee"
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-    drawTilemap(ctx)
-  }
-
-  function drawTilemap(ctx: CanvasRenderingContext2D) {
-    const xGap = (sideLength / view.zoom) * 1.5
-    const yGap = (apothem / view.zoom) * 2
-
-    // Optimisation: get the range of tiles that are visible on screen
-    // (Not perfect due to yOffset for even columns, but good enough for now)
-    const leftmostX = Math.floor((view.x - sideLength) / xGap)
-    const rightmostX = Math.ceil((view.x + sideLength + canvas.width / view.zoom) / xGap)
-    const topmostY = Math.floor((view.y - apothem) / yGap)
-    const bottommostY = Math.ceil((view.y + apothem + canvas.height / view.zoom) / yGap)
-
-    // Optimisation: create the hexagon path only once
-    const hexPath = getHexPath()
-
-    // Loop through only the coords of tiles that are visible and draw the tiles
-    for (let x = leftmostX; x < rightmostX; x++) {
-      for (let y = topmostY; y < bottommostY; y++) {
-        const tile = tilemap.getTile(x, y)
-        if (tile) {
-          const yOffset = x % 2 === 1 ? 0 : apothem / view.zoom
-
-          // Calculate the center of the hexagon in world coordinates
-          const worldX = x * xGap + sideLength
-          const worldY = y * yGap + apothem + yOffset
-
-          // Apply zoom and view offset
-          const screenX = (worldX - view.x) * view.zoom
-          const screenY = (worldY - view.y) * view.zoom
-
-          drawHex(ctx, hexPath, screenX, screenY, tile)
-        }
-      }
-    }
-
-    function getHexPath() {
-      const hexPath = new Path2D()
-
-      // Draw hexagon, starting from top left vertex, moving clockwise
-      hexPath.moveTo(-sideLength / 2, -apothem)
-      hexPath.lineTo(+sideLength / 2, -apothem)
-      hexPath.lineTo(+sideLength, 0)
-      hexPath.lineTo(+sideLength / 2, +apothem)
-      hexPath.lineTo(-sideLength / 2, +apothem)
-      hexPath.lineTo(-sideLength, 0)
-      hexPath.closePath()
-
-      return hexPath
-    }
-  }
-
-  function drawHex(
-    ctx: CanvasRenderingContext2D,
-    hexPath: Path2D,
-    centerX: number,
-    centerY: number,
-    tile: Tile
-  ) {
-    // Save current context state to restore later
-    ctx.save()
-
-    // Translate to hexagon center
-    ctx.translate(centerX, centerY)
-
-    // Fill hexagon
-    ctx.fillStyle = "#07bb07"
-    ctx.fill(hexPath)
-
-    // Outline hexagon
-    ctx.strokeStyle = "#000000"
-    ctx.lineWidth = 1
-    ctx.stroke(hexPath)
-
-    // Draw hex coordinates on hexagon center
-    ctx.fillStyle = "#000000"
-    ctx.font = `${24 * view.zoom}px Consolas`
-    ctx.textAlign = "center"
-    ctx.textBaseline = "middle"
-    ctx.fillText(`${tile.x},${tile.y}`, 0, 0)
-
-    // Restore context state
-    ctx.restore()
+    drawTilemap(ctx, canvas, view, tilemap, sideLength, apothem)
   }
 </script>
 
---
+++ src/modules/GameWorld/canvasTypes.ts
@@ -0,0 +1,5 @@
+export interface View {
+  x: number
+  y: number
+  zoom: number
+}
